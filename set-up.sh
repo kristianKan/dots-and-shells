15afc764382f9877b8e24c216a5f6ce13b548815
Signal Noise <signalnoise@Digital.local>
2021-01-13 12:08:12 +0000

    add bash_profile

--- set-up.sh
+++ set-up.sh
@@ -18,3 +18,10 @@ if test -f "$TERN"; then
 else
   echo ".tern-project is missing"
 fi
+
+BASH_PROFILE=.bash_profile
+if test -f "$BASH_PROFILE"; then
+  cp .bash_profile ~/
+else
+  echo ".bash_profile is missing"
+fi
